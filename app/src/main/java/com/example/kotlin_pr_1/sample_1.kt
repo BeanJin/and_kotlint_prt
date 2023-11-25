76ed03b2bd348bae4a746e01d98978884ccc295c
LHJ <lhj7234@naver.com>
2023-11-25 23:13:57 +0900

    git commit
    23 11 25 안드로이드 툴을 이용한 코틀린 공부
    
    1. 조건식 사용법
    2. 배열 공부

--- app/src/main/java/com/example/kotlin_pr_1/sample_1.kt
+++ app/src/main/java/com/example/kotlin_pr_1/sample_1.kt
@@ -1,16 +1,19 @@
 package com.example.kotlin_pr_1
 
 fun main() {
-   helloworld()
 
-    println(add(4, 5))
+    checkNum(90)
+   // helloworld()
+
+    // println(add(4, 5))
 
     // 3. String Template
 
-    val name = "홍진"
-    val lastName = " 이"
-    println("my name is ${name + lastName}") // <<<< 파이썬의 {}과 비슷함 / 항상 대괄호 넣는다고 생각하기
-    println("이건 2\$a다") // \넣으면 기호 사용할 수 있다
+    // val name = "홍진"
+    // val lastName = " 이"
+    // println("my name is ${name + lastName}") // <<<< 파이썬의 {}과 비슷함 / 항상 대괄호 넣는다고 생각하기
+    // println("이건 2\$a다") // \넣으면 기호 사용할 수 있다
+
 }
 
 // 1. 함수
@@ -38,4 +41,63 @@ fun hi(){
     var d = 102
 
     var name = "홍진"
+}
+
+// 4. 조건식
+
+fun maxBy(a : Int, b : Int) : Int {
+    if(a > b) {
+        return a
+    }
+    else{
+        return b
+    }
+}
+
+fun maxBy2(a : Int, b : Int) = if(a > b) a else b
+
+// 이부분은 Statement
+fun checkNum(score : Int) {
+    when(score) {
+        0 -> println("이건 0")
+        1 -> println("이건 1")
+        2, 3-> println("이건 2 아님 3")
+    }
+
+    // 이부분은 Expression / 코틀린의 모든 함수는 Expression
+
+    var b = when(score) {
+        1 -> 1
+        2 -> 2
+        else -> 3
+    }
+
+    println("b : ${b}")
+
+    when(score){
+        in 90..100 -> println("똑똑하네")
+        in 10..80 -> println("적당하네")
+        else -> println("큰일났네")
+    }
+}
+
+// 5 Array / List
+
+// Array
+
+// List 1. Imutable << 수정X List 2. MutableList << 수정O
+
+fun array(){
+    val array = arrayOf(1, 2, 3)
+    val list = listOf(1, 2, 3)
+
+    val array2 = arrayOf(1, "d", 3.4f)
+    val list2 = listOf(1, "d", 11L)
+
+    array[0] = 3
+    var result = list.get(0)
+
+    val arrayList = arrayListOf<Int>()
+    arrayList.add(10)
+    arrayList.add(20) // list a<<< 이걸 가르킴 list b
 }
